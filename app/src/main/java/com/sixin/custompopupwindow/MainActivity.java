3ee6cc36d72f6143b8847401aef40ec38a1df729
CUTYlife <2055305346@qq.com>
2017-07-23 11:12:53 +0800

    构建今日头条popupWindow

---
+++ app/src/main/java/com/sixin/custompopupwindow/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sixin.custompopupwindow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity  {
+
+   // TODO: 2017/7/23  实现今日头条popupWindow效果
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
